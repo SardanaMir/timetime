c13330fc51175a856cea671a5e3893c3055ac798
SardanaMir <sardanadord@yandex.ru>
2023-10-10 14:10:48 +0300

    DRY code
    deleted the repeated code

--- script.js
+++ script.js
@@ -3,61 +3,36 @@ const yakutskMinutes = document.querySelector('.yakutskMinutes');
 
 const petersburgHours = document.querySelector('.petersburgHours');
 const petersburgMinutes = document.querySelector('.petersburgMinutes');
+const petersburgSeconds = document.querySelector('.petersburgSeconds');
 
 const bishkekHours = document.querySelector('.bishkekHours');
 const bishkekMinutes = document.querySelector('.bishkekMinutes');
 const bishkekSeconds = document.querySelector('.bishkekSeconds');
 
+const asia = 'Asia';
+const europe = 'Europe';
 
-setInterval(function time(){
-    fetch("https://worldtimeapi.org/api/timezone/Asia/Bishkek")
+//interval
+const oneSec = 1000;
+
+function time(url, area, city, hours, minutes, seconds){
+    fetch(`${url}/${area}/${city}`)
         .then(response => response.json())
         .then(data => {
-        let bishkek = data['datetime']
-
-        let bishkekHoursData = `${bishkek[11]}${bishkek[12]}`
-        bishkekHours.innerHTML = bishkekHoursData
-
-        let bishkekMinutesData = `${bishkek[14]}${bishkek[15]}`
-        bishkekMinutes.innerHTML = bishkekMinutesData
-
-        // let bishkekSecondsData = `${bishkek[17]}${bishkek[18]}`
-        // bishkekSeconds.innerHTML = bishkekSecondsData
-
+        let datetime = data['datetime']
+        hours.innerHTML = `${datetime[11]}${datetime[12]}`;
+        minutes.innerHTML = `${datetime[14]}${datetime[15]}`;
+        // seconds.innerHTML = `${datetime[17]}${datetime[18]}`;
         })
         .catch(error => {
             console.log(error);
         });
+}
 
-    fetch("https://worldtimeapi.org/api/timezone/Asia/Yakutsk")
-        .then(response => response.json())
-        .then(data => {
-        let yakutskTime = data['datetime']
+setInterval (() => {
+    time(URL, europe, 'Moscow', petersburgHours, petersburgMinutes)
+    time(URL, asia, 'Yakutsk', yakutskHours, yakutskMinutes)
+    time(URL, asia, 'Bishkek', bishkekHours, bishkekMinutes)
+}, oneSec);
 
-        let yakutskHoursData = `${yakutskTime[11]}${yakutskTime[12]}`
-        yakutskHours.innerHTML = yakutskHoursData
-
-        let yakutskMinutesData = `${yakutskTime[14]}${yakutskTime[15]}`
-        yakutskMinutes.innerHTML = yakutskMinutesData
-        })
-        .catch(error => {
-        console.log(error);
-        });
-
-    fetch("https://worldtimeapi.org/api/timezone/Europe/Moscow")
-        .then(response => response.json())
-        .then(data => {
-        let petersburgTime = data['datetime']
-
-        let petersburgHoursData = `${petersburgTime[11]}${petersburgTime[12]}`
-        petersburgHours.innerHTML = petersburgHoursData
-
-        let petersburgMinutesData = `${petersburgTime[14]}${petersburgTime[15]}`
-        petersburgMinutes.innerHTML = petersburgMinutesData
-        })
-        .catch(error => {
-        console.log(error);
-        });
-
-},1000)
 
